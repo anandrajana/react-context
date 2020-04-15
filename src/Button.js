cf62f64a9736cd35adc70edb2e2694511b409584
Anand Rajana <Anand.Rajana@sigma.se>
2020-04-15 16:41:31 +0530

    default consumer

--- src/Button.js
+++ src/Button.js
@@ -1,19 +1,19 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./theme";
 
-// class ThemedButton extends React.Component {
-//   render() {
-//     let props = this.props;
-//     let theme = this.context;
-//     return <button {...props} style={{ backgroundColor: theme.background }} />;
-//   }
-// }
-// ThemedButton.contextType = ThemeContext;
-
-function ThemedButton(props) {
-  let theme = useContext(ThemeContext);
-  return <button {...props} style={{ backgroundColor: theme.background }} />;
+class ThemedButton extends React.Component {
+  render() {
+    let props = this.props;
+    let theme = this.context;
+    return <button {...props} style={{ backgroundColor: theme.background }} />;
+  }
 }
+ThemedButton.contextType = ThemeContext;
+
+// function ThemedButton(props) {
+//   let theme = useContext(ThemeContext);
+//   return <button {...props} style={{ backgroundColor: theme.background }} />;
+// }
 
 // function ThemedButton(props) {
 //   return (
